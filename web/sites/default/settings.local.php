15043d87d5a8166c884bc2ac106e42fc19f832bb
Fede <fedeanton@gmail.com>
2020-11-03 19:20:48 -0300

    Módulo Linked field

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/development.services.yml';
+
 $databases['default']['default'] = array (
   'database' => 'drupal_nt_local',
   'username' => 'demo',
